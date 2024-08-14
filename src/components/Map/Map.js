fd74d74427f6117f89f942b2f18e4455cfbcc606
Paul Laird <plaird83@gmail.com>
2024-08-14 12:58:16 -0400

    Initial working map with nav button

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -1,24 +1,32 @@
+import React, { useEffect } from 'react';
 import './Map.css';
-// import Navbar from "../shared/Navbar";
 
 function Map() {
+    useEffect(() => {
+        // Add the class to the body when the component mounts
+        document.body.classList.add('homepage');
+
+        // Remove the class from the body when the component unmounts
+        return () => {
+            document.body.classList.remove('homepage');
+        };
+    }, []);
+
     return (
-        <fragment className="map">
-            <div className="FormPage">
-                <iframe
-                    src="https://www.google.com/maps/d/u/3/embed?mid=1hLSMlnfBjDCZfcG24lCtvB379JuDZZ8&ehbc=2E312F"
-                    className="dynamic-height-iframe"
-                    width="100%"
-                    frameBorder="0"
-                    marginHeight="0"
-                    marginWidth="0"
-                    title="Find a College Parking Spot"
-                    id="map"
-                >
-                    Loading…
-                </iframe>
-            </div>
-        </fragment>
+        <div className="map">
+            <iframe
+                src="https://www.google.com/maps/d/u/3/embed?mid=1hLSMlnfBjDCZfcG24lCtvB379JuDZZ8&ehbc=2E312F"
+                className="dynamic-height-iframe"
+                width="100%"
+                frameBorder="0"
+                marginHeight="0"
+                marginWidth="0"
+                title="Find a College Parking Spot"
+                id="map"
+            >
+                Loading…
+            </iframe>
+        </div>
     );
 }
 
--- src/components/shared/Navbar/index.js
+++ src/components/shared/Navbar/index.js
@@ -9,30 +9,30 @@ const Navbar = () => {
     const toggleMenu = () => setShowMenu(!showMenu)
 
     return (
-        <div className='navbar'>
-            <div className='logo'>
-                <FaMapMarkerAlt id="logo" />
+        <div>
+            <div className='floating-button' onClick={toggleMenu}>
+                {showMenu ? <FaTimes /> : <FaBars />}
             </div>
-            <ul className={showMenu ? 'nav-menu active' : 'nav-menu'}>
-                <Link to="/"><li className='nav-item'>Home</li></Link>
-                <Link to="/tenant-register"><li className='nav-item'>Find Parking</li></Link>
-                <Link to="/host-register"><li className='nav-item'>Host Parking</li></Link>
-                <Link
-                    to="#"
-                    onClick={(e) => {
-                        window.location.href = "mailto:info@findcollegeparking.com?subject=FindCollegeParking.com"
-                        e.preventDefault();
-                    }}
-                >
-                    <li className='nav-item'>Contact</li>
-                </Link>
-            </ul>
-            <div className='hamburger' onClick={toggleMenu}>
-                {showMenu ? (<FaTimes size={30} style={{ color: '#f8f8f8' }} />) : (<FaBars size={30} style={{ color: '#f8f8f8' }} />)}
-
-            </div>
+            {showMenu && (
+                <div className='nav-menu'>
+                    <ul>
+                        <Link to="/"><li className='nav-item'>Home</li></Link>
+                        <Link to="/tenant-register"><li className='nav-item'>Find Parking</li></Link>
+                        <Link to="/host-register"><li className='nav-item'>Host Parking</li></Link>
+                        <Link
+                            to="#"
+                            onClick={(e) => {
+                                window.location.href = "mailto:info@findcollegeparking.com?subject=FindCollegeParking.com"
+                                e.preventDefault();
+                            }}
+                        >
+                            <li className='nav-item'>Contact</li>
+                        </Link>
+                    </ul>
+                </div>
+            )}
         </div>
     )
 }
 
-export default Navbar
+export default Navbar;
